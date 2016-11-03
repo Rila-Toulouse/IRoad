91e8fe6ec459e35bd8992b2024be2118cbdd328f
remy delmas <remy.cool@hotmail.fr>
2016-11-03 10:52:35 +0100

    Geolocalisation et affichage en temps réél de la position sur la carte

---
+++ scripts/logo.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+
+
+// function getrealcontentheight() {
+   // var header = $.mobile.activepage.find("div[data-role='header']:visible");
+   // var footer = $.mobile.activepage.find("div[data-role='footer']:visible");
+   // var content = $.mobile.activepage.find("div[data-role='content']:visible:visible");
+   // var viewport_height = $(window).height();
+
+   // var content_height = viewport_height - header.outerheight() - footer.outerheight();
+   // if ((content.outerheight() - header.outerheight() - footer.outerheight()) <= viewport_height) {
+       // content_height -= (content.outerheight() - content.height());
+   // }
+   // return content_height;
+// }
